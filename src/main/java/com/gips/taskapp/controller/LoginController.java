29ca42442c46da58f023fad0bfcf2de630679c64
g2603-yamamoto <g2603-yamamoto@outlook.jp>
2026-04-20 16:21:07 +0900

    loginコントローラとサービスクラスを作りました

---
+++ src/main/java/com/gips/taskapp/controller/LoginController.java
@@ -0,0 +1,10 @@
+package com.gips.taskapp.controller;
+
+public class LoginController {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ src/main/java/com/gips/taskapp/service/LoginService.java
@@ -0,0 +1,10 @@
+package com.gips.taskapp.service;
+
+public class LoginService {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
