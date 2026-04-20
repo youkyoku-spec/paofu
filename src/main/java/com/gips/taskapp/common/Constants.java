601d291267ec817d794c7f7bb101f2a707a399a0
g2604-yang <g2604-yang@outlook.jp>
2026-04-20 18:26:35 +0900

    タスク一覧画面_共通関数

---
+++ src/main/java/com/gips/taskapp/common/Constants.java
@@ -0,0 +1,17 @@
+package com.gips.taskapp.common;
+
+/**
+ * 定数
+ *
+ */
+public final class Constants {
+
+	private Constants() {
+	}
+
+	// リーダー
+	public static final String LEADER = "leader";
+
+	// メンバー
+	public static final String MEMBER = "member";
+}
