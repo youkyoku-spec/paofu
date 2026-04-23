8a6900874c41cd49e1276cd3613afeb3a5bd1dcc
g2603-yamamoto <g2603-yamamoto@outlook.jp>
2026-04-23 15:36:01 +0900

    チームレビュー指摘修正

--- src/main/java/com/gips/taskapp/service/LoginService.java
+++ src/main/java/com/gips/taskapp/service/LoginService.java
@@ -2,8 +2,11 @@ package com.gips.taskapp.service;
 
 import com.gips.taskapp.dto.UserDto;
 
+/**
+ * 認証サービスインターフェース
+ */
 public interface LoginService {
-
+	//一致したログインIDとパスワードがあった場合はユーザー情報を、無かった場合はエラーメッセージを返す
 	public UserDto loginService(String loginId, String password);
 
 }
