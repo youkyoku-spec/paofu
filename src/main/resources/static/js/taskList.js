fa5d39ed8cd5231c663bf66e4cee661a6b4f1e37
g2604-yang <g2604-yang@outlook.jp>
2026-04-27 16:13:39 +0900

    タスク一覧画面_削除機能障害修正

--- src/main/resources/static/js/taskList.js
+++ src/main/resources/static/js/taskList.js
@@ -1,12 +1,18 @@
 let actionUrl = "";
 
+// 削除
 function openDeleteModal(taskId) {
-    actionUrl = "/deleteTask?taskId=" + taskId;
     document.getElementById("modalMessage").innerText = "本当に削除しますか？";
-    document.getElementById("modalOk").onclick = goAction;
+
+    document.getElementById("modalOk").onclick = function () {
+        document.getElementById("deleteTaskId").value = taskId;
+        document.getElementById("deleteForm").submit();
+    };
+
     document.getElementById("confirmModal").style.display = "flex";
 }
 
+// ログアウト
 function openLogoutModal() {
     actionUrl = "/logout";
     document.getElementById("modalMessage").innerText = "ログアウトしますか？";
@@ -22,6 +28,7 @@ function closeModal() {
     document.getElementById("confirmModal").style.display = "none";
 }
 
+// プルダウン
 function changeStatus(select) {
     select.form.submit();
 }
